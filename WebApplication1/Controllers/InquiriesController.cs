7eed41b950db191a3f2818bf6ac69cade3e315d2
shoshe.2020sh@gmail.com <shoshe.2020sh@gmail.com>
2020-12-02 18:19:16 +0200

    commit

--- WebApplication1/Controllers/InquiriesController.cs
+++ WebApplication1/Controllers/InquiriesController.cs
@@ -15,13 +15,15 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult Charges()
+        public ActionResult Charges(int meter)
         {
+            ViewBag.meter = meter;
             return View();
         }
 
-        public ActionResult Transfers()
+        public ActionResult Transfers(int meter)
         {
+            ViewBag.meter = meter;
             return View();
         }
 
@@ -37,6 +39,60 @@ namespace WebApplication1.Controllers
                 amount += topup.Amount;
             }
             ViewBag.amount = amount;
+            return View();
+        }
+
+       public ActionResult Transferom(int meter,Transfer[] transfer)
+        {
+            int rc;
+            int count = 0;
+            string[] otps = null;
+            Topup[] topups = Topup.GetTopups(new TopupParameters { }, out rc);//for topups
+            Topup[] topups1 = null;//for senders otp
+
+            foreach (Transfer transfer1 in transfer)
+            {
+                foreach (Topup topup1 in topups)
+                {
+                    if(transfer1.SenderOTP==topup1.OTP)
+                    {
+                        topups1[count] = topup1;
+                        count += 1;
+                    }
+                }
+
+            }
+            int count1 = 0;
+            int count2 = 0;//for otps
+            foreach (Topup topup in topups1)
+            {
+               
+                if (topups1[count1].MeterId == meter)
+                {
+                    otps[count2] = topups1[0].OTP;
+                    count2 += 1;
+                }
+                count1 += 1;
+            }
+
+            Transfer[] transfers = null;// for senders
+            int count3 = 0;
+            if (otps != null)
+            {
+             foreach( string otp  in otps)
+                {
+                    foreach(Transfer transfer1 in transfer)
+                    {
+                        if (otp == transfer1.SenderOTP)
+                        {
+                            transfers[count3] = transfer1;
+                            count3 += 1;
+                        }
+                    }
+                }
+            }
+
+            ViewBag.transfers = transfers;
             return View();
         }
 
