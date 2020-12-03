336488d186869d55de951e7648bf0af9f5375e6a
shoshe.2020sh@gmail.com <shoshe.2020sh@gmail.com>
2020-12-03 21:05:48 +0200

    commit

--- WebApplication1/Controllers/ChargeMeterController.cs
+++ WebApplication1/Controllers/ChargeMeterController.cs
@@ -11,56 +11,67 @@ namespace WebApplication1.Controllers
     public class ChargeMeterController : Controller
     {
         // GET: ChargeMeter
-        public ActionResult Index()
+        public ActionResult Index(string error)
         {
+            ViewBag.error = error;
             return View();
         }
 
         public ActionResult Charge(string otp)
         {
-            Customer customer = (Session["customer"] as Customer);
+            Customer customer = null;
             int rc;
             Topup[] topup = Topup.GetTopups(new TopupParameters { OTP = otp }, out rc);
-            Meter[] meters = Meter.GetMeters(new MeterParameters { Meterid = topup[0].MeterId }, out rc);
-            if (Session["customer"] != null)
+            if (topup[0].Status == "0")
             {
-                if (customer.Id == meters[0].UserId)
+                if (Session["customer"] != null)
                 {
-                    SMS sms = new SMS();
-                    sms.To_number = customer.Telephone;
-                    sms.Msg = $"  أهلا وسهلا بك أنت تحاول الان شحن عدادك باستخدام موقعنا في الشركة ورقم الكود الذي يريد شحنه هو {otp}";
-                    string status = sms.Send();
-                    if (status == "OK")
+                    Meter[] meters = Meter.GetMeters(new MeterParameters { Meterid = topup[0].MeterId }, out rc);
+                    customer = (Session["customer"] as Customer);
+                    if (customer.Id == meters[0].UserId)
                     {
-                        topup[0].Charged();
-                    }
-                    else
-                    {
-                        return RedirectToAction("index", "ChargeMeter");
-                    }
-                }
+                        SMS sms = new SMS();
+                        sms.To_number = customer.Telephone;
+                        sms.Msg = $"  أهلا وسهلا بك أنت تحاول الان شحن عدادك باستخدام موقعنا في الشركة ورقم الكود الذي يريد شحنه هو {otp}";
+                        string status = sms.Send();
 
-                else if (customer.Id != meters[0].UserId)
-                {
-                    Customer customer1 = new Customer(meters[0].UserId);
-                    SMS sms = new SMS();
-                    sms.To_number = customer1.Telephone;
-                    sms.Msg = $"يحاول {customer.name} شحن عدادك باستخدام موقنا في الشركة ورقم الكود الذي يريد شحنه هو {otp}";
-                    string status = sms.Send();
-                    if (status == "OK")
-                    {
-                        topup[0].Charged();
+                        if (status == "OK")
+                        {
+                            topup[0].Charged();
+                        }
+                        else
+                        {
+                            return RedirectToAction("index", "ChargeMeter", new { error = "4" });
+                        }
                     }
-                    else
+
+                    else if (customer.Id != meters[0].UserId)
                     {
-                        return RedirectToAction("index", "ChargeMeter");
+                        Customer customer1 = new Customer(meters[0].UserId);
+                        SMS sms = new SMS();
+                        sms.To_number = customer1.Telephone;
+                        sms.Msg = $"يحاول {customer.name} شحن عدادك باستخدام موقنا في الشركة ورقم الكود الذي يريد شحنه هو {otp}";
+                        string status = sms.Send();
+                        if (status == "OK")
+                        {
+                            topup[0].Charged();
+                        }
+                        else
+                        {
+                            return RedirectToAction("index", "ChargeMeter",new { error = "4" });
+                        }
                     }
+                    
                 }
-                return View();
+                else if (Session["customer"] == null)
+                {
+                    return RedirectToAction("index", "ChargeMeter", new { error = "2" });
+                }
+             return View();
             }
-            else
+            else 
             {
-                return RedirectToAction("index", "ChargeMeter");
+                return RedirectToAction("index", "ChargeMeter", new { error = "3" });
             }
         }
     }
